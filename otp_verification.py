17117a4061ec0de3bc245943dbac6fbbb23106d1
Scott Petrovcic <69741804+DancesWithDobes@users.noreply.github.com>
2023-03-01 17:50:37 -0500

    changes

--- otp_verification.py
+++ otp_verification.py
@@ -3,18 +3,26 @@
 import smtplib
 import random
 
-
-
+optLength = 6
 otp = ""
 
+'''
 for num in range(0,6):
-    otp += str(randint(0,9))
-
-
-
-
-
-
+    otp += str(random.randint(0,9))
 
 print(otp)
 
+#  otpMessage = 
+'''
+
+
+def get_otp_number(length):
+
+    output = ""
+
+    for num in range(0,length):
+        output += str(random.randint(0,length))
+    
+    return output
+
+opt = get_otp_number(optLength)
